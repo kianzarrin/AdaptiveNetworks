60fba76797e86e6d1233a7829a2f0a3956985593
kian.zarrin <kian.zarrin@gmail.com>
2020-10-03 12:30:31 +0300

    polish

--- AdvancedRoads/Patches/RoadEditor/AssetEditorRoadUtilsPatch.cs
+++ AdvancedRoads/Patches/RoadEditor/AssetEditorRoadUtilsPatch.cs
@@ -3,7 +3,6 @@ namespace AdvancedRoads.Patches.RoadEditor {
     using HarmonyLib;
     using KianCommons;
     using PrefabIndeces;
-    using System.Reflection;
     using static KianCommons.Assertion;
 
     // AssetEditorRoadUtils
@@ -13,25 +12,26 @@ namespace AdvancedRoads.Patches.RoadEditor {
     public static class AssetEditorRoadUtilsPatch {
         [HarmonyPostfix]
         [HarmonyPatch("AddArrayElement")]
-        static void AddArrayElement_Postfix(object target, FieldInfo field, object newObject) {
+        static void AddArrayElement_Postfix(object target) {
             int found = 0;
 
+            // brute force search all arrays to find teh one that is the given our target.
             foreach (NetInfo info in NetInfoExt.EditNetInfos) {
                 var InfoExt = info.GetExt();
 
-                if (target == info.m_nodes) {
+                if (ReferenceEquals(target, info.m_nodes)) {
                     found++;
                     Assert(info.m_nodes.Length == InfoExt.NodeInfoExts.Length + 1, "new nodes == old nodes + 1");
                     var item = new NetInfoExt.Node(info.m_nodes.Last());
                     InfoExt.NodeInfoExts.AppendElement(item);
                 }
-                if (target == info.m_segments) {
+                if (ReferenceEquals(target, info.m_segments)) {
                     found++;
                     Assert(info.m_segments.Length == InfoExt.SegmentInfoExts.Length + 1, "new segments == old segments + 1");
                     var item = new NetInfoExt.Segment(info.m_segments.Last());
                     InfoExt.SegmentInfoExts.AppendElement(item);
                 }
-                if (target == info.m_lanes) {
+                if (ReferenceEquals(target, info.m_lanes)) {
                     found++;
                     Assert(info.m_lanes.Length == InfoExt.LaneInfoExts.Length + 1, "new lanes == old lanes + 1");
                     var item = new NetInfoExt.Lane(info.m_lanes.Last());
@@ -40,7 +40,7 @@ namespace AdvancedRoads.Patches.RoadEditor {
                 for (int laneIndex = 0; laneIndex < info.m_lanes.Length; ++laneIndex) {
                     var props = info.m_lanes[laneIndex].m_laneProps.m_props;
                     var propsExt = InfoExt.LaneInfoExts[laneIndex].PropInfoExts;
-                    if (target == props) {
+                    if (ReferenceEquals(target, props)) {
                         found++;
                         Assert(props.Length == propsExt.Length + 1, "new lane props == old lane props + 1");
                         var item = new NetInfoExt.LaneProp(props.Last());
@@ -49,11 +49,12 @@ namespace AdvancedRoads.Patches.RoadEditor {
                 }
             }
             Assert(found == 1, $"only 1 element is expected to be added at a time. got {found}");
+            // EventObjectModified invokes ReExtendEditedPrefabIndeces() which fixes all indeces.
         }
 
         [HarmonyPrefix]
         [HarmonyPatch("RemoveArrayElement")]
-        static void RemoveArrayElement_Prefix(object element, object target, FieldInfo field) {
+        static void RemoveArrayElement_Prefix(object element) {
             if (element is NetInfoExtension.Node node) {
                 var array = NetInfoExt.Buffer[node.PrefabIndex].NodeInfoExts;
                 int i = node.Index;
@@ -71,6 +72,7 @@ namespace AdvancedRoads.Patches.RoadEditor {
                 int i = prop.Index;
                 array.DropElement(i);
             }
+            // EventObjectModified invokes ReExtendEditedPrefabIndeces() which fixes all indeces.
         }
     }
 }
