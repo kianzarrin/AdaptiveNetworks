8f2c75c392e0d09e528d5ef1db52cfb3854937cf
kian.zarrin <kian.zarrin@gmail.com>
2020-12-24 11:58:22 +0200

    set default mesh import scale to 100

---
+++ AdaptiveRoads/Patches/RoadEditor/AssetEditorRoadUtilsPatch - Copy.cs
@@ -0,0 +1,21 @@
+namespace AdaptiveRoads.Patches.RoadEditor.x {
+    using ColossalFramework.UI;
+    using HarmonyLib;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using static KianCommons.ReflectionHelpers;
+
+    // set default scale to 100
+    [HarmonyPatch]
+    public static class AssetImporterAssetImport_ScalePatch {
+        static IEnumerable<MethodBase> TargetMethods() {
+            yield return GetMethod(typeof(AssetImporterAssetImport), "Awake");
+            yield return GetMethod(typeof(AssetImporterAssetImport), "ResetTransformFields");
+            yield return GetMethod(typeof(AssetImporterAssetImport), "SetDefaultScale");
+        }
+        static void Postfix(UITextField ___m_Scale) {
+            if (___m_Scale.text == "1") // if CalculateDefaultScale() returned 1
+                ___m_Scale.text = "100";
+        }
+    }
+}
