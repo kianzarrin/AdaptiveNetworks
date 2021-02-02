f7f1e326fff6b88367c1840a7fddc8b0c5e8364d
kian.zarrin <kian.zarrin@gmail.com>
2021-02-02 23:06:37 +0200

    patch network detective to show AR flags

--- AdaptiveRoads/API.cs
+++ AdaptiveRoads/API.cs
@@ -6,7 +6,25 @@ namespace AdaptiveRoads {
     using AdaptiveRoads.Manager;
 
     public static class API {
+        static NetworkExtensionManager man => NetworkExtensionManager.Instance;
+
         public static bool IsAdaptive(this NetInfo info) =>
             NetInfoExtionsion.IsAdaptive(info);
+
+#pragma warning disable HAA0601 // Value type to reference type conversion causing boxing allocation
+        public static object GetARSegment(ushort id) => man.SegmentBuffer[id];
+        public static object GetARNode(ushort id) => man.NodeBuffer[id];
+        public static object GetARSegmentEnd(ushort segmentID, ushort nodeID)
+            => man.GetSegmentEnd(segmentID, nodeID);
+        public static object GetARSegmentEnd(ushort segmentID, bool startNode)
+            => man.GetSegmentEnd(segmentID, startNode);
+
+        public static object GetARSegmentFlags(ushort id) => man.SegmentBuffer[id].m_flags;
+        public static object GetARNodeFlags(ushort id) => man.NodeBuffer[id].m_flags;
+        public static object GetARSegmentEndFlags(ushort segmentID, ushort nodeID)
+            => man.GetSegmentEnd(segmentID, nodeID).m_flags;
+        public static object GetARSegmentEndFlags(ushort segmentID, bool startNode)
+            => man.GetSegmentEnd(segmentID, startNode).m_flags;
+#pragma warning restore HAA0601 
     }
 }
