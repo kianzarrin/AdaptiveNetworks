eb33df9b6eb336c1305c45a72ad94a4b56ce0ec9
kian.zarrin <kian.zarrin@gmail.com>
2021-03-07 14:09:29 +0200

    warn about ns2 in road editor

--- AdaptiveRoads/Patches/metadata/OnLoadPatch.cs
+++ AdaptiveRoads/Patches/metadata/OnLoadPatch.cs
@@ -1,14 +1,25 @@
 namespace AdaptiveRoads.Patches.metadata {
+    using ColossalFramework.UI;
     using HarmonyLib;
-    using AdaptiveRoads.Manager;
+    using KianCommons.Plugins;
+
 
     [HarmonyPatch(typeof(LoadAssetPanel), "OnLoad")]
     [HarmonyPriority(100)]
     public static class OnLoadPatch {
         public static void Postfix() {
-            if (ToolsModifierControl.toolController.m_templatePrefabInfo is NetInfo) {
+            if(ToolsModifierControl.toolController.m_templatePrefabInfo is NetInfo) {
+                if(PluginUtil.GetNetworkSkins().IsActive()) {
+                    UIView.library
+                          .ShowModal<ExceptionPanel>("ExceptionPanel")
+                          .SetMessage(
+                              "Incompatible mod",
+                              "NS2 is incompatible with AR in Road editor.",
+                              false);
+                }
+
                 // don't know why but the lods do not load the first time i load an asset.
-                NetManager.instance.RebuildLods(); 
+                NetManager.instance.RebuildLods();
             }
         }
     }
