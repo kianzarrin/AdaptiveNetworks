51369ba8fa41a5bfa27a920da93884cbd6c49d60
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2021-04-11 02:58:47 +0200

    first working draft.

---
+++ QuayRoadsTemp/Patcher.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+namespace QuayRoadsMod {
+    public static class Patcher {
+        private const string HarmonyId = "xlf1024.QuayRoads";
+
+        private static bool patched = false;
+
+        public static void PatchAll() {
+            if (patched) return;
+
+            UnityEngine.Debug.Log("QuayRoadsMod: Patching...");
+
+            patched = true;
+
+            // Apply your patches here!
+            Harmony.DEBUG = true;
+            var harmony = new Harmony("xlf1024.QuayRoads");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void UnpatchAll() {
+            if (!patched) return;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.UnpatchAll(HarmonyId);
+
+            patched = false;
+
+            UnityEngine.Debug.Log("QuayRoadsMod: Reverted...");
+        }
+    }
+	/*
+    // Random example patch
+    [HarmonyPatch(typeof(SimulationManager), "CreateRelay")]
+    public static class SimulationManagerCreateRelayPatch {
+        public static void Prefix() {
+            UnityEngine.Debug.Log("CreateRelay Prefix");
+        }
+    }
+
+    // Random example patch
+    [HarmonyPatch(typeof(LoadingManager), "MetaDataLoaded")]
+    public static class LoadingManagerMetaDataLoadedPatch {
+        public static void Prefix() {
+            UnityEngine.Debug.Log("MetaDataLoaded Prefix");
+        }
+    }*/
+}
---
+++ QuayRoadsTemp/QuayRoadsMod.cs
@@ -0,0 +1,24 @@
+﻿using CitiesHarmony.API;
+using ICities;
+// Make sure that "using HarmonyLib;" does not appear here!
+// Only reference HarmonyLib in code that runs when Harmony is ready (DoOnHarmonyReady, IsHarmonyInstalled)
+
+namespace QuayRoadsMod {
+    public class Mod : IUserMod {
+        // Make sure that HarmonyLib is not referenced in any way in your IUserMod implementation!
+        // Instead, apply your patches from a separate static patcher class!
+        // (otherwise it will fail to instantiate the type when CitiesHarmony is not installed)
+
+        public string Name => "Quay Roads Mod";
+        public string Description => "Trying to port the terrain deforming behaviour of quays to roads";
+
+        public void OnEnabled() {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled() {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+}
+
