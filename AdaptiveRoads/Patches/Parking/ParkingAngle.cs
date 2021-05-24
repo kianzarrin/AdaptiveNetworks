bb033b7a74fa5aa8c2a7778146f3644f5d8ff00b
kian.zarrin <kian.zarrin@gmail.com>
2021-05-24 19:02:49 +0300

    patched parking angle

--- AdaptiveRoads/Patches/Parking/ParkingAngle.cs
+++ AdaptiveRoads/Patches/Parking/ParkingAngle.cs
@@ -1,75 +1,71 @@
 namespace AdaptiveRoads.Patches.Parking {
-    using ColossalFramework;
     using HarmonyLib;
     using JetBrains.Annotations;
     using KianCommons;
     using System.Reflection;
     using UnityEngine;
-    using static ColossalFramework.Math.VectorUtils;
-    using AdaptiveRoads.Util;
     using AdaptiveRoads.Manager;
     using KianCommons.Patches;
     using System;
     using System.Collections.Generic;
     using System.Reflection.Emit;
-    using static KianCommons.Patches.TranspilerUtils;
-    using System.Diagnostics;
-    using KianCommons.Math;
-    using ColossalFramework.Math;
+    using System.Linq;
 
     [InGamePatch]
     [UsedImplicitly]
-    [HarmonyPatch(typeof(PassengerCarAI))]
+    [HarmonyPatch(typeof(PassengerCarAI), "FindParkingSpaceRoadSide")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "HAA0601:Value type to reference type conversion causing boxing allocation", Justification = "not performance critical")]
     static class ParkingAngle {
-        delegate bool FindPathPosition(Vector3 position, ItemClass.Service service, NetInfo.LaneType laneType, VehicleInfo.VehicleType vehicleType, bool allowUnderground, bool requireConnect, float maxDistance, out PathUnit.Position pathPos);
-        static MethodInfo mFindPathPosition = DeclaredMethod<FindPathPosition>(typeof(PathManager));
+        //delegate bool FindPathPosition(Vector3 position, ItemClass.Service service, NetInfo.LaneType laneType, VehicleInfo.VehicleType vehicleType, bool allowUnderground, bool requireConnect, float maxDistance, out PathUnit.Position pathPos);
+        //static MethodInfo mFindPathPosition = DeclaredMethod<FindPathPosition>(typeof(PathManager));
+        static MethodInfo mGetLaneID => typeof(PathManager).GetMethod(nameof(PathManager.GetLaneID), throwOnError: true);
+        static MethodInfo mFixValues => typeof(ParkingAngle).GetMethod(nameof(ParkingAngle.FixValues), throwOnError: true);
 
-        [HarmonyTranspiler]
-        [HarmonyPatch("FindParkingSpaceRoadSide")]
-        static IEnumerable<CodeInstruction> FindParkingSpaceRoadSideTranspiler(IEnumerable<CodeInstruction> instructions) {
-            foreach(var code in instructions) {
-                yield return code;
-                if (code.Calls("Normalize")) {
-                    yield return new CodeInstruction(OpCodes.Ldloc_3); // laneID
-                    yield return new CodeInstruction(OpCodes.Ldloca_S, 16); // direction (output from CalculatePositionAndDirection)
-                    var mRotate = typeof(ParkingAngle).GetMethod(nameof(Rotate), throwOnError: true);
-                    yield return new CodeInstruction(OpCodes.Call, mRotate);
-                } 
+        static float angle_;
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase origin) {
+            try {
+                var codes = instructions.ToList();
+                var loadRefPathPos = codes.Find(c => c.IsLdLocA(typeof(PathUnit.Position), out _));
+                var loadRefWidth = new CodeInstruction(OpCodes.Ldarga_S, origin.GetArgLoc("width"));
+                var loadRefLength = new CodeInstruction(OpCodes.Ldarga_S, origin.GetArgLoc("length"));
+                var callFixValues = new CodeInstruction(OpCodes.Call, mFixValues);
+                var iCallGetLaneID = codes.FindIndex(c => c.Calls(mGetLaneID));
+                codes.InsertInstructions(iCallGetLaneID, new CodeInstruction[]{
+                    // laneID is already on the stack
+                    loadRefPathPos.Clone(),
+                    loadRefWidth,
+                    loadRefLength,
+                    callFixValues,
+                });
+                return codes;
+            } catch (Exception ex) {
+                ex.Log();
+                return instructions;
             }
         }
 
-        public static void Rotate(uint laneID, ref Vector3 v) {
-            ushort segmentID = laneID.ToLane().m_segment;
-            float angle = segmentID.ToSegment().Info.GetMetaData()?.ParkingAngleDegrees ?? 0;
-            if (angle == 0)
-                return;
-
-            var v2 = v.ToCS2D().RotateRadCCW(-Mathf.Deg2Rad * angle);
-            v.x = v2.x;
-            v.z = v2.y;
+        static void Postfix(ref Quaternion parkRot) {
+            if (angle_ != 0) {
+                Rotate(ref parkRot, angle_);
+            }
+            angle_ = 0;
         }
+
+        public static uint FixValues(uint laneID, ref PathUnit.Position pathPos, ref float width, ref float length) {
+            angle_ = laneID.ToLane().m_segment.ToSegment().Info.GetMetaData()?.ParkingAngleDegrees ?? 0;
+            if (angle_ > 30) {
+                width = FixWidth(length, angle_);
+                length = FixLength(width, angle_);
+            }
+            return laneID;
+        }
+
+        public static void Rotate(ref Quaternion parkRot, float parkingAngleRad) =>
+            parkRot *= Quaternion.Euler(0, parkingAngleRad, 0);
 
         // patch this :float num8 = (info.m_lanes[(int)pathPos.m_lane].m_width - **width**) * 0.5f;
-        public static float Width(float parkingAngleRad, float carLength) => carLength / Mathf.Cos(parkingAngleRad);
-        public static float Length(float parkingAngleRad, float carWidth) => carWidth / Mathf.Cos(parkingAngleRad);
-
-
-
-
-
-    }
-
-    [InGamePatch]
-    [UsedImplicitly]
-    static class CheckOverlapPatch {
-        public delegate bool CheckOverlap(ushort ignoreParked, ref Bezier3 bezier, float offset, float length, out float minPos, out float maxPos);
-        static MethodBase TargetMethod() => DeclaredMethod<CheckOverlap>(typeof(PassengerCarAI));
-
-        static void Prefix(ref float length) {
-
-        }
-            
-
+        public static float FixWidth(float parkingAngleRad, float carLength) => carLength / Mathf.Cos(parkingAngleRad);
+        public static float FixLength(float parkingAngleRad, float carWidth) => carWidth / Mathf.Cos(parkingAngleRad);
 
     }
 }
