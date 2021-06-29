1b142d02474af757f1020294f7bc2135ca9340a9
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2021-06-29 21:43:56 +0200

    Patch all overrides as well

--- AdaptiveRoads/Patches/QuayRoads/ModifyMaskPatch.cs
+++ AdaptiveRoads/Patches/QuayRoads/ModifyMaskPatch.cs
@@ -16,7 +16,7 @@ namespace AdaptiveRoads.Patches {
     static class ModifyMaskPatch {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NetAI), "SegmentModifyMask")]
-        static bool SegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, ref RoadAI __instance) {
+        static bool SegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, NetAI __instance) {
             var net = __instance.m_info.GetMetaData();
             if (net is null) return true;
 
@@ -27,10 +27,33 @@ namespace AdaptiveRoads.Patches {
             return ModifyMask(profile, invert, index, ref surface, ref heights, ref edges, ref left, ref right, ref leftStartY, ref rightStartY, ref leftEndY, ref rightEndY, ref __result);
 
         }
+        #region apply SegmentModifyMaskPrefix to all overrides too
+        //note for refactoring: some overrides use rightT and leftT as argument name instead of right and left
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CanalAI), "SegmentModifyMask")]
+        static bool CanalAISegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, CanalAI __instance) {
+            return SegmentModifyMaskPrefix(segmentID, ref data, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftStartY, ref rightStartY, ref leftEndY, ref rightEndY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FloodWallAI), "SegmentModifyMask")]
+        static bool FloodWallAISegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, FloodWallAI __instance) {
+            return SegmentModifyMaskPrefix(segmentID, ref data, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftStartY, ref rightStartY, ref leftEndY, ref rightEndY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PedestrianWayAI), "SegmentModifyMask")]
+        static bool PedestrianWayAISegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, PedestrianWayAI __instance) {
+            return SegmentModifyMaskPrefix(segmentID, ref data, index, ref surface, ref heights, ref edges, ref left, ref right, ref leftStartY, ref rightStartY, ref leftEndY, ref rightEndY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(QuayAI), "SegmentModifyMask")]
+        static bool QuayAISegmentModifyMaskPrefix(ushort segmentID, ref NetSegment data, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result, QuayAI __instance) {
+            return SegmentModifyMaskPrefix(segmentID, ref data, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftStartY, ref rightStartY, ref leftEndY, ref rightEndY, ref __result, __instance);
+        }
+        #endregion
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(NetAI), "NodeModifyMask")]
-        static bool NodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftY, ref float rightY, ref bool __result, ref RoadAI __instance) {
+        static bool NodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftY, ref float rightY, ref bool __result, NetAI __instance) {
             var net = __instance.m_info.GetMetaData();
             if (net is null) return true;
 
@@ -56,7 +79,54 @@ namespace AdaptiveRoads.Patches {
             }
             return result;
         }
-
+        #region apply NodeModifyMaskPrefix to all overrides too
+        //note for refactoring: some overrides use rightT and leftT as argument name instead of right and left
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CableCarPathAI), "NodeModifyMask")]
+        static bool CableCarPathAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, CableCarPathAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CanalAI), "NodeModifyMask")]
+        static bool CanalAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, CanalAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FloodWallAI), "NodeModifyMask")]
+        static bool FloodWallAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, FloodWallAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PedestrianPathAI), "NodeModifyMask")]
+        static bool PedestrianPathAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftY, ref float rightY, ref bool __result, PedestrianPathAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref left, ref right, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PedestrianWayAI), "NodeModifyMask")]
+        static bool PedestrianWayAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float left, ref float right, ref float leftY, ref float rightY, ref bool __result, PedestrianWayAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref left, ref right, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PowerLineAI), "NodeModifyMask")]
+        static bool PowerLineAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, PowerLineAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(QuayAI), "NodeModifyMask")]
+        static bool QuayAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, QuayAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(RoadBaseAI), "NodeModifyMask")]
+        static bool RoadBaseAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, RoadBaseAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SupportCableAI), "NodeModifyMask")]
+        static bool SupportCableAINodeModifyMaskPrefix(ushort nodeID, ref NetNode data, ushort segment1, ushort segment2, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftY, ref float rightY, ref bool __result, SupportCableAI __instance) {
+            return NodeModifyMaskPrefix(nodeID, ref data, segment1, segment2, index, ref surface, ref heights, ref edges, ref leftT, ref rightT, ref leftY, ref rightY, ref __result, __instance);
+        }
+        #endregion
 
 
         static bool ModifyMask(ProfileSection[] profile, bool invert, int index, ref TerrainModify.Surface surface, ref TerrainModify.Heights heights, ref TerrainModify.Edges edges, ref float leftT, ref float rightT, ref float leftStartY, ref float rightStartY, ref float leftEndY, ref float rightEndY, ref bool __result) {
