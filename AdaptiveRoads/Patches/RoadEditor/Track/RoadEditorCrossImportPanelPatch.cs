1977d661173466791cf04694eb8cbba41ed3d37d
kianzzarrin <kian.zarrin@gmail.com>
2021-11-22 13:26:42 +0200

    fixed cross import.

--- AdaptiveRoads/Patches/RoadEditor/Track/RoadEditorCrossImportPanelPatch.cs
+++ AdaptiveRoads/Patches/RoadEditor/Track/RoadEditorCrossImportPanelPatch.cs
@@ -7,7 +7,7 @@ namespace AdaptiveRoads.Patches.RoadEditor.Track {
     using UnityEngine;
     using KianCommons;
     using ColossalFramework.UI;
-    using System;
+    using System.Linq;
 
     [HarmonyPatch(typeof(RoadEditorCrossImportPanel))]
     public static class RoadEditorCrossImportPanelPatch {
@@ -27,21 +27,21 @@ namespace AdaptiveRoads.Patches.RoadEditor.Track {
             }
         }
 
-        public static HashSet<NetInfo.Segment> TrackModels = new HashSet<NetInfo.Segment>();
         [HarmonyPatch("PopulateModelList")]
         [HarmonyPostfix]
         public static void PopulateModelListPostfix(
             UIListBox ___m_ModelListbox,
             UIListBox ___m_RoadListbox,
             UIButton ___m_ImportButton,
-            ref NetInfo[] ___m_FilteredRoads,
+            NetInfo[] ___m_FilteredRoads,
             ref RoadEditorCrossImportPanel.ModelWrapper[] ___m_FilteredModels) {
             NetInfo netInfo = ___m_FilteredRoads[___m_RoadListbox.selectedIndex];
 
             var infoExt = netInfo?.GetMetaData();
             if((infoExt?.Tracks).IsNullorEmpty()) return;
 
-            TrackModels.Clear();
+            var modelWrappers = new List<RoadEditorCrossImportPanel.ModelWrapper>(10);
+            var listboxItems = new List<string>(10);
             foreach(var trackInfo in infoExt.Tracks) {
                 if(trackInfo.m_material.shader ) {
                     var segmentInfo = new NetInfo.Segment {
@@ -50,18 +50,15 @@ namespace AdaptiveRoads.Patches.RoadEditor.Track {
                         m_lodMaterial = trackInfo.m_lodMaterial,
                         m_lodMesh = trackInfo.m_lodMesh,
                     };
-                    TrackModels.Add(segmentInfo);
-                    ___m_FilteredModels.AddItem(new RoadEditorCrossImportPanel.ModelWrapper(segmentInfo));
+                    var modelWrapper = new RoadEditorCrossImportPanel.ModelWrapper(segmentInfo);
+                    modelWrappers.Add(modelWrapper);
+                    listboxItems.Add(modelWrapper.Name + " (Track)");
                 }
             }
+            if(modelWrappers.Count == 0) return;
 
-            var oldItems = ___m_ModelListbox.items;
-            ___m_ModelListbox.items = new string[___m_FilteredModels.Length+TrackModels.Count];
-            Array.Copy(oldItems, ___m_ModelListbox.items, oldItems.Length);
-            for(int i = oldItems.Length; i < ___m_ModelListbox.items.Length; ++i) {
-                var trackWrapper = ___m_FilteredModels[i];
-                ___m_ModelListbox.items[i] = trackWrapper.Name + " (Track)";
-            }
+            ___m_FilteredModels = ___m_FilteredModels.Concat(modelWrappers).ToArray(); ;
+            ___m_ModelListbox.items = ___m_ModelListbox.items.Concat(listboxItems).ToArray();
 
             if(___m_ModelListbox.items.Length > 0) {
                 ___m_ModelListbox.selectedIndex = 0;
