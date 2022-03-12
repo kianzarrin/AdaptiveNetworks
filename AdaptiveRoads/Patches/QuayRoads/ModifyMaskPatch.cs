d0f88e82e312035769f7e6cf6a05788f7fb124c9
xlf1024 <33392024+xlf1024@users.noreply.github.com>
2022-03-12 20:55:46 +0100

    Correct namespace for QuayRoads ModifyMask patch

--- AdaptiveRoads/Patches/QuayRoads/ModifyMaskPatch.cs
+++ AdaptiveRoads/Patches/QuayRoads/ModifyMaskPatch.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using static KianCommons.ReflectionHelpers;
 using JetBrains.Annotations;
 
-namespace AdaptiveRoads.Patches {
+namespace AdaptiveRoads.Patches.QuayRoads {
 
     [HarmonyPatch]
     [PreloadPatch]
