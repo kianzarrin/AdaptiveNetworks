f44f59fdfb3455da1fb3d8192282007c932fb138
kianzzarrin <kian.zarrin@gmail.com>
2022-04-19 20:33:05 +0300

    fix build error

--- AdaptiveRoads/Util/PackageManagerUtil.cs
+++ AdaptiveRoads/Util/PackageManagerUtil.cs
@@ -22,26 +22,24 @@ namespace AdaptiveRoads.Util {
         /// throws exception if no matching package was found.
         /// </summary>
         public static Package[] GetLoadingPackages() {
-            get {
-                try {
-                    if (AssetDataExtension.ListingMetaData != null) {
-                        Log.Debug($"package from ListingMetaData({AssetDataExtension.ListingMetaData.name}) is {AssetDataExtension.ListingMetaData?.assetRef.package}");
-                        var ret = AssetDataExtension.ListingMetaData?.assetRef.package
-                            ?? throw new Exception($"ListingMetaData?.assetRef.package is null");
-                    }
-                    string name = AssetDataExtension.CurrentBasicNetInfo.name;
-                    int dotIndex = name.LastIndexOf('.');
-                    if (dotIndex > 0) {
-                        Assertion.Assert(dotIndex > 0, $"dotIndex:{dotIndex} > 0");
-                        string packageName = name.Substring(0, dotIndex);
-                        return GetPackages(packageName);
-                    } else {
-                        throw new Exception("could not analyze package name for NetInfo " + AssetDataExtension.CurrentBasicNetInfo);
-                    }
-                } catch (Exception ex) {
-                    ex.Log($"failed to get package for CurrentBasicNetInfo='{AssetDataExtension.CurrentBasicNetInfo}' and ListingMetaData='{AssetDataExtension.ListingMetaData}'");
-                    throw ex;
+            try {
+                if (AssetDataExtension.ListingMetaData != null) {
+                    Log.Debug($"package from ListingMetaData({AssetDataExtension.ListingMetaData.name}) is {AssetDataExtension.ListingMetaData?.assetRef.package}");
+                    var ret = AssetDataExtension.ListingMetaData?.assetRef.package
+                        ?? throw new Exception($"ListingMetaData?.assetRef.package is null");
                 }
+                string name = AssetDataExtension.CurrentBasicNetInfo.name;
+                int dotIndex = name.LastIndexOf('.');
+                if (dotIndex > 0) {
+                    Assertion.Assert(dotIndex > 0, $"dotIndex:{dotIndex} > 0");
+                    string packageName = name.Substring(0, dotIndex);
+                    return GetPackages(packageName);
+                } else {
+                    throw new Exception("could not analyze package name for NetInfo " + AssetDataExtension.CurrentBasicNetInfo);
+                }
+            } catch (Exception ex) {
+                ex.Log($"failed to get package for CurrentBasicNetInfo='{AssetDataExtension.CurrentBasicNetInfo}' and ListingMetaData='{AssetDataExtension.ListingMetaData}'");
+                throw ex;
             }
         }
 
