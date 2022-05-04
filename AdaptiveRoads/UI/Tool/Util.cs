ce4d8d66ec069bbb91b08e33fe89427f7c49665b
kianzzarrin <kian.zarrin@gmail.com>
2022-05-04 15:10:50 +0300

    bugfix: multi select gets stuck in loop,

--- AdaptiveRoads/UI/Tool/Util.cs
+++ AdaptiveRoads/UI/Tool/Util.cs
@@ -2,6 +2,7 @@ namespace AdaptiveRoads.UI.Tool {
     using AdaptiveRoads.Util;
     using KianCommons;
     using System.Collections.Generic;
+    using System.Linq;
 
     public static class Util {
         public static IEnumerable<ushort> GetSimilarSegmentsBetweenJunctions(ushort segmentID) {
@@ -12,15 +13,29 @@ namespace AdaptiveRoads.UI.Tool {
             return ret;
         }
 
+        private static HashSet<ushort> hashset = new ();
         static IEnumerable<ushort> TraverseSegments(ushort startSegmentID, ushort nodeID) {
+            NetInfo startInfo = startSegmentID.ToSegment().Info;
             ushort nextSegmentID = startSegmentID;
+            int watchdog = 0;
             while(nodeID != 0 && nodeID.ToNode().m_flags.IsFlagSet(NetNode.Flags.Middle | NetNode.Flags.Bend)) {
                 nextSegmentID = nodeID.ToNode().GetAnotherSegment(nextSegmentID);
-                if(nextSegmentID.ToSegment().Info != startSegmentID.ToSegment().Info)
-                    yield break;
-                yield return nextSegmentID;
+
+                if (watchdog++ > 10000)
+                    break;
+                if (nextSegmentID.ToSegment().Info != startInfo)
+                    break;
+                if(nextSegmentID == startSegmentID)
+                    break; // circled around
+                if (hashset.Contains(nextSegmentID)) {
+                    Log.Error("unexpected Loop detected. send screenshot of networks to kian.");
+                    break;
+                }
+
+                hashset.Add(nextSegmentID);
                 nodeID = nextSegmentID.ToSegment().GetOtherNode(nodeID);
             }
+            return hashset;
         }
 
         public static IEnumerable<LaneData> GetSimilarLanes(LaneData lane, IEnumerable<ushort> segmentIDs_) =>
