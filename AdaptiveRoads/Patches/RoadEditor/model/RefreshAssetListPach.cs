bb00bf1d8b59069af477a64755914ce2e30b2fdb
kianzzarrin <kian.zarrin@gmail.com>
2022-06-30 14:23:11 +0300

    wider model import panel

--- AdaptiveRoads/Patches/RoadEditor/model/RefreshAssetListPach.cs
+++ AdaptiveRoads/Patches/RoadEditor/model/RefreshAssetListPach.cs
@@ -20,12 +20,13 @@ namespace AdaptiveRoads.Patches.RoadEditor.AssetImporterAssetImportPatches {
             try {
                 if (ToolsModifierControl.toolController.m_editPrefabInfo is NetInfo) {
                     // increase panel size:
-                    __instance.component.width = 1115;
-                    ___m_FileList.width = 550;
+                    float delta = 400;
+                    __instance.component.width = 1115 + delta;
+                    ___m_FileList.width = 550 + delta;
                     ___m_FileList.relativePosition =
                         ___m_FileList.relativePosition.SetI(0, 0);
                     ___m_SmallPreview.relativePosition =
-                        ___m_SmallPreview.relativePosition.SetI(600, 0);
+                        ___m_SmallPreview.relativePosition.SetI(600 + delta, 0);
                 }
                 AddSubfolders(___m_FileList, extensions);
             } catch(Exception ex) {
