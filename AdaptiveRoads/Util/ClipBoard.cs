71dfc3cb346f0ba5ab8e180c08ce741c6bf50807
kianzzarrin <kian.zarrin@gmail.com>
2022-09-26 23:39:17 +0300

    count clipboard items

--- AdaptiveRoads/Util/ClipBoard.cs
+++ AdaptiveRoads/Util/ClipBoard.cs
@@ -16,56 +16,68 @@ namespace AdaptiveRoads.Util {
             }else if (target is NetLaneProps.Prop prop) {
                 SourceInfo = prop.GetParent(out int laneIndex, out _);
                 SourceLane = SourceInfo?.m_lanes?[laneIndex];
+                Log.Debug($"SourceInfo set to {SourceInfo}, laneInex={laneIndex}");
             } else if( target is NetInfo.Node node) {
                 SourceInfo = node.GetParent(out _);
                 SourceLane = null;
+                Log.Debug($"SourceInfo set to {SourceInfo}");
             } else if (target is NetInfo.Segment segment) {
                 SourceInfo = segment.GetParent(out _);
                 SourceLane = null;
+                Log.Debug($"SourceInfo set to {SourceInfo}");
             } else {
                 throw new NotImplementedException(target.ToString());
             }
         }
 
+        public static int Count { get; private set; }
+
         public static bool HasData<T>() {
             return Data is T || (Data is IEnumerable<T> e && e.Any()) ;
         }
+
 
         public static void SetData(IEnumerable<NetLaneProps.Prop> props) {
             // The output of Select() is only valid as long as props is alive.
             // but if props goes out of scope, I need to have a copy for my self.
             // that is why I convert to array.
-            Data = props.Select(prop=>prop.Clone()).ToArray();
             SetSource(props.FirstOrDefault());
-
-            int n = (Data as IEnumerable<NetLaneProps.Prop>).Count();
-            Log.Debug("ClipBoard.SetData() -> Data.count=" + n /*+ Environment.StackTrace*/);
+            var data = props.Select(prop => prop.Clone()).ToArray();
+            Data = data;
+            Count = data.Length;
+            Log.Debug("ClipBoard.SetData() -> Data.count=" + Count /*+ Environment.StackTrace*/);
         }
 
         public static void SetData(NetLaneProps.Prop prop) {
-            Data = prop.Clone();
             SetSource(prop);
+            Data = prop.Clone();
+            Count = 1;
             Log.Debug("ClipBoard.SetData() -> Data=" + Data /*+ Environment.StackTrace*/);
         }
 
         public static void SetData(IEnumerable<NetInfo.Node> nodes) {
             SetSource(nodes.FirstOrDefault());
-            Data = nodes.Select(node => node.Clone()).ToArray();
+            var data = nodes.Select(node => node.Clone()).ToArray();
+            Data = data;
+            Count = data.Length;
         }
 
         public static void SetData(NetInfo.Node node) {
             SetSource(node);
             Data = node.Clone();
+            Count = 1;
         }
 
         public static void SetData(IEnumerable<NetInfo.Segment> segments) {
             SetSource(segments.FirstOrDefault());
             Data = segments.Select(node => node.Clone()).ToArray();
+            Count = (Data as Array).Length;
         }
 
         public static void SetData(NetInfo.Segment segment) {
             SetSource(segment);
             Data = segment.Clone();
+            Count = 1;
         }
 
         public static Array GetDataArray() {
